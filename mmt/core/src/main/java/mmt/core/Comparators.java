6fd986c893dcb308923a4f100451e23dd002ff02
Ole Erik Nordtømme <oleerik.nord@gmail.com>
2022-09-29 11:00:15 +0200

    deleted main method and written doc on methods

--- mmt/core/src/main/java/mmt/core/Comparators.java
+++ mmt/core/src/main/java/mmt/core/Comparators.java
@@ -6,49 +6,30 @@ import java.util.Comparator;
 import java.util.List;
 
 public class Comparators {
-    
+
+
+    /**
+     * 
+     * @return a comparator that compares the movies based on their rating. This means that the movie/movies with the highest rating will come first in the list.
+     */
     public static Comparator<Movie> sortByHighestRating(){
         return Comparator.comparing(Movie::getRatingNumber).reversed();
     }
 
+     /**
+     * 
+     * @return a comparator that compares the movies based on their title. The list will be sorted alfabetical.
+     */
     public static Comparator<Movie> sortByTitle(){
         return Comparator.comparing(Movie::getTitle);
     }
 
+     /**
+     * 
+     * @return a comparator that compares the movies based on their duration. The movies with least duration will come first.
+     */
     public static Comparator<Movie> sortByDuration(){
         return Comparator.comparing(Movie::getDuration);
     }
-
-
-
-    public static void main(String[] args) {
-        Time time = new Time(3, 20, 0);
-        Time time1 = new Time(2, 21, 0);
-        Time time2 = new Time(2, 20, 0);
-        Movie movie = new Movie("aaaa", time2);
-        Movie movie2 = new Movie("hade", time1);
-        Movie movie4 = new Movie("x", time);
-        Movie movie3 = new Movie("hei", time);
-        Rating rating = new Rating(2);
-        Rating rating2 = new Rating(7);
-        Rating rating3 = new Rating(3);
-        Rating rating4 = new Rating(8);
-        movie.setRating(rating);
-        movie2.setRating(rating2);
-        movie3.setRating(rating3);
-        movie4.setRating(rating4);
-        List<Movie> movies = new ArrayList<>();
-        movies.add(movie);
-        movies.add(movie2);
-        movies.add(movie3);
-        movies.add(movie4);
-        movies.sort(sortByHighestRating());
-        System.out.println(movies);
-/*         movies.stream().map(p -> p.getRating()).sorted(sortByHighestRating()).toList();
- *//*         List <IRating> ratings = new ArrayList<>(movies.stream().map(p -> p.getRating()).toList());
-
- */    
-    }
-
     
 }
