a3a031951eb72dff2b1abc07f86ef80a031fc66f
vebjohaug <vebjohau@stud.ntnu.no>
2022-11-13 19:23:21 +0100

    get and delete endpoint for movie object

--- mmt/rest/src/main/java/mmt/restapi/MmtService.java
+++ mmt/rest/src/main/java/mmt/restapi/MmtService.java
@@ -6,10 +6,13 @@ import org.slf4j.LoggerFactory;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
+import mmt.core.IMovie;
 import mmt.core.MovieList;
 import mmt.json.MyMovieConfig;
 
@@ -27,10 +30,10 @@ public class MmtService {
     public MovieList getMovieList(){
         try{
             MovieList movieList = myMovieConfig.loadMovieList();
-            LOG.debug("List of movies to get:" + movieList.toString());
+            LOG.debug("MovieList successfully loaded: " + movieList.toString());
             return movieList;
         } catch (Exception e){
-            LOG.debug("The service failed to get MovieList, returning a new MovieList.");
+            LOG.debug("The service failed to load MovieList, returning a new MovieList.");
             return new MovieList();
         }
     }
@@ -39,7 +42,38 @@ public class MmtService {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public void putMovieList(MovieList movieList){
-        LOG.debug("List of movie to put:" + movieList.toString());
+        LOG.debug("MovieList successfully updated:" + movieList.toString());
         myMovieConfig.saveMovieList(movieList);
     }
+
+   
+    @GET
+    @Path("/{movie}")
+    public IMovie getMovie(@PathParam("movie") String movie) {
+      IMovie movieFromMovieList = getMovieList().getMovie(movie);
+      if (movieFromMovieList == null){
+        LOG.debug("requested movie does not exist!");
+      }
+      else{
+        LOG.debug("getting movie " + movie);
+      }
+      return getMovieList().getMovie(movie);
+    }
+
+    @DELETE
+    @Path("/{movie}")
+    public void deleteMovie(@PathParam("movie") String movie) {
+      MovieList movieListFromFile = getMovieList();
+      IMovie movieToDelete = movieListFromFile.getMovie(movie);
+
+      if (movieToDelete == null){
+        LOG.debug("the movie requested to delete does not exist!");
+      }
+      else{
+        movieListFromFile.removeMovie(movieToDelete);
+        putMovieList(movieListFromFile);
+        LOG.debug("movie " + movie + " successfully deleted");
+      }
+    }
+
 }
